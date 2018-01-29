78c2ad76cf3b756a0d8713a17dc567085c7206da
sergio_kag <sergio_kagiema@live.com>
2018-01-29 12:15:48 +0200

    Setting up the skeleton for the plugin

--- js/night_mode.js
+++ js/night_mode.js
@@ -16,6 +16,42 @@
 
     'use strict';
 
-    //...code
+     /* Declaring the variables
+      * 
+      * 
+      * 
+      * 
+      */ 
+      const window = root;
+      const document = root.document;
+      const NightMode = {};
+      let supports  = 'querySelector' in document && 'addEventListener' in window; // Testing features!
+  
+      
+      
+      
+      
+      //code ....
+  
+  
+      
+      
+      // Public API
+      NightMode.destroy = function() {
+        alert('destroy the plugin')
+      };    
+  
+      NightMode.init = function() {
+        alert('init the plugin')
+      };
+  
+      document.onreadystatechange = function () {
+        if (document.readyState == "interactive") {
+    
+            //NightMode.init();
+        }
+      }
+  
+      return NightMode;
 
 }));
