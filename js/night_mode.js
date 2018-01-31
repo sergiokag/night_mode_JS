04c7bff0051b1bc8420fb019050a2180977ac611
sergio_kag <sergio_kagiema@live.com>
2018-01-31 04:44:35 +0200

    Complete the basic structure of the Plugin || Add css/image folder

--- js/night_mode.js
+++ js/night_mode.js
@@ -14,31 +14,223 @@
 
 } )( typeof global !== 'undefined' ? global : this.window || this.global, ( function (root) {
 
+    /**
+     * Night Mode Js
+     * @version 0.0.1
+     */
+
     'use strict';
 
      /* Declaring the variables
-      * 
-      * 
-      * 
-      * 
-      */ 
-      const window = root;
-      const document = root.document;
-      const NightMode = function () {
+     * 
+     * 
+     * 
+     * 
+     */ 
+    const window = root;
+    const document = root.document;
+    let supports  = 'querySelector' in document && 'addEventListener' in window; // Testing features!
+    
+    
+    
+    
+    
+    const NightMode = function (options) {
+
+
+
+
+
+
+          
+      /**
+       * If not optios have been passed
+       * then use {}
+       * @private
+       */
+      options = options || {};
+      
+
+
+
+
+
+                
+      /**
+       * Variable to store the daytime
+       * @private
+       */
+      let _daytime = null;
+      
+
+
+
+
+
+          
+      /**
+       * All selected DOM elements for text color .
+       * @private
+       */
+      const $elsColor = [];
+
+
+
+
+
+
+            
+      /** 
+       * All selected DOM elements for background-color .
+       * @private
+       */
+      const $elsBgColor = [];
+
+
+
+
+
+
+            
+      /**
+       * 
+       * Check if day or night
+       * @private
+       */        
+      const checkIfDayOrNight =  function() {
+      
+        let now = new Date();
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        let second = now.getSeconds();
+        const isDayTime = hour > 7 && hour < 19;
+      
+      
+        if (isDayTime) {
+
+          _datime = false;           
+
+        } else {
+
+          _datime = true;            
+
+        }
         
+        setTimeout(checkIfDayOrNight, 1000);
       };
-      let supports  = 'querySelector' in document && 'addEventListener' in window; // Testing features!
-      
-      
-      
-      
-      //code ....
-  
-  
-      
-      
-      // Public API
-  
+
+
+
+
+
+
+            
+      /**
+       * Default options for the plugin.
+       * @private
+       */         
+      const _defaults = {
+        isNight: false,
+        autoplay: false,
+        customColors: null,
+      };
+
+
+
+
+
+
+            
+      /**
+       * 
+       * Custom extend function
+       * @private
+       */
+      const extend = function() {
+          let key, i;
+    
+          for (i = 1; i < arguments.length; i++)
+          {
+              for (key in arguments[i])
+              {
+                  if (arguments[i].hasOwnProperty(key))
+                      arguments[0][key] = arguments[i][key];
+              }
+          }
+    
+          return arguments[0];
+      };
+
+
+
+
+
+
+              
+      /**
+       * Current options set by the caller including defaults.
+       * @private
+       */
+      // TO DO CHRISTOS
+      const _options = extend({}, _defaults , options);
+
+
+
+
+
+
+      //   rest of the private code
+
+
+
+
+
+
+                      
+    };
+    // end of the function contructor
+
+
+
+
+
+
+            
+      //API
+
+
+
+
+
+
+            
+      // TO DO DIMITRIS
+      NightMode.prototype.destroy = function() {
+        alert('destroy')
+      };
+
+
+
+
+
+
+            
+      NightMode.prototype.init = function() {
+        alert('init')
+      };
+
+
+
+
+
+
+            
       return NightMode;
 
+
+
+
+
+
+            
 }));
